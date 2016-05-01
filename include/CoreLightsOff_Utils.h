7bf2d04af8da9c842b92d9fb583916b17b01a48c
n2omatt <n2o.matt@gmail.com>
2016-05-01 02:20:22 -0300

    - Update the version numbers.

--- include/CoreLightsOff_Utils.h
+++ include/CoreLightsOff_Utils.h
@@ -52,9 +52,9 @@
 
 //The core version number.
 #define COW_CORELIGHTSOFF_VERSION_MAJOR    "0"
-#define COW_CORELIGHTSOFF_VERSION_MINOR    "5"
-#define COW_CORELIGHTSOFF_VERSION_REVISION "1"
+#define COW_CORELIGHTSOFF_VERSION_MINOR    "6"
+#define COW_CORELIGHTSOFF_VERSION_REVISION "0"
 
-#define COW_CORELIGHTSOFF_VERSION "0.5.1"
+#define COW_CORELIGHTSOFF_VERSION "0.6.0"
 
 #endif // defined(__CoreLightsOff_include_CoreLightsOff_Utils_h__) //
